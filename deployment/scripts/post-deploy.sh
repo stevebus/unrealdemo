c8a90436e2e56ac650e5d9059ec659b5c092c564
Marvin Garcia <magar@microsoft.com>
2021-10-05 09:14:49 -0400

    removed unused section from post deployment script

--- deployment/scripts/post-deploy.sh
+++ deployment/scripts/post-deploy.sh
@@ -6,15 +6,6 @@ egname=$3
 # install/update azure iot az cli extension
 az extension add --name azure-iot -y
 
-# # create ADT endpoint and route for signalR integration
-# eptest=$(az dt endpoint show --dt-name $adtname --en "$egname-ep" 2>null)
-# if [ -z $eptest ];
-# then
-#   az dt endpoint create eventgrid --dt-name $adtname --eventgrid-resource-group $rgname --eventgrid-topic $egname --endpoint-name "$egname-ep"
-# else
-#   echo 'endpoint exists, skipping creation'
-# fi
-
 rttest=$(az dt route show --dt-name $adtname --rn "$egname-rt" 2> null)
 if [ -z $rttest];
 then
